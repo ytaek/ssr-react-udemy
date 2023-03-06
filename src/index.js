0f2ad456e4664ec32da97caecaa166025ee78aa7
ytaek.kim <ytaek.kim@hcil.snu.ac.kr>
2023-03-07 00:57:09 +0900

    section2

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+const app = express();
+
+app.get('/', (req, res) => {
+	const content = renderToString(<Home />);
+	
+	res.send(content);
+});
+
+
+app.listen(3001, () => {
+	console.log('Listening on prot 3001');
+});
+
+
